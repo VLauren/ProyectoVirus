ac50e221edf3c46358e3e005d9384ba2a1f8df93
LVilchez <lau.16.9@gmail.com>
2017-04-22 16:55:17 +0200

    movimiento jugador e instanciar disparos

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jugador : MonoBehaviour {
+public class Jugador : MonoBehaviour
+{
+    public static float VELOCIDAD_MAXIMA = 4;
+    public static float ACCELERACION = 0.2f;
+    public static float VELOCIDAD_ROTACION = 150;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject disparo;
+
+    Vector3 inercia = Vector3.zero;
+
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        // Movimiento
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            // accelerar
+            inercia += transform.forward * (ACCELERACION + Time.deltaTime);
+            inercia.x = Mathf.Clamp(inercia.x, -VELOCIDAD_MAXIMA, VELOCIDAD_MAXIMA);
+            inercia.z = Mathf.Clamp(inercia.z, -VELOCIDAD_MAXIMA, VELOCIDAD_MAXIMA);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            transform.Rotate(0, Time.deltaTime * VELOCIDAD_ROTACION, 0);
+        if (Input.GetAxisRaw("Horizontal") < 0)
+            transform.Rotate(0, - Time.deltaTime * VELOCIDAD_ROTACION, 0);
+
+        transform.Translate(inercia * Time.deltaTime, Space.World);
+
+        // Disparo
+
+        if (Input.GetButtonDown("Fire1"))
+            if (disparo != null)
+                Instantiate(disparo, transform.position + transform.forward, transform.rotation);
+
+    }
 }
